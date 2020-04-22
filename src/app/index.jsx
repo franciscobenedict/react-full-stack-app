1e027918757beedd3a740f2d572da47c5bd673c3
Francisco Benedict <franciscobenedict@Franciscos-office-iMac-Conrad-Office.local>
2020-04-22 23:51:04 +0100

    Working on my new app:

---
+++ src/app/index.jsx
@@ -0,0 +1,13 @@
+import { store } from "./store";
+
+console.log(store.getState());
+
+import React from 'react';
+import ReactDom from 'react-dom';
+//import { Dashboard } from './components/Dashboard';
+import { Main } from './components/Main';
+
+ReactDom.render(
+  < Main />,
+  document.getElementById("app")
+);
