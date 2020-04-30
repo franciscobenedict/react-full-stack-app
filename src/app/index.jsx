2a20a9678eb6ef64c39d4ec6bd23c1f22aad92c2
Francisco Benedict <franciscobenedict@Franciscos-office-iMac-Conrad-Office.local>
2020-04-30 15:45:03 +0100

    add support for SCSS stylesheets

--- src/app/index.jsx
+++ src/app/index.jsx
@@ -1,10 +1,11 @@
-import { store } from "./store";
-
-console.log(store.getState());
-
+import { store } from "./store";console.log(store.getState());
 import React from 'react';
 import ReactDom from 'react-dom';
 //import { Dashboard } from './components/Dashboard';
+//import './index.scss';
+import './global';
+import styles from './app.module';
+
 import { Main } from './components/Main';
 
 ReactDom.render(
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,8 @@
 const path = require("path");
+const isDevelopment = process.env.NODE_ENV === 'development';
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+console.log('isDevelopment', isDevelopment);
 
 module.exports = {
   mode: "development",
@@ -9,15 +13,52 @@ module.exports = {
     publicPath:'/'
   },
   resolve: {
-    extensions: ['.js','.jsx']
+    extensions: ['.js', '.jsx', '.scss', '.css']
   },
   devServer: {
     historyApiFallback: true
   },
   module: {
-    rules: [{
-      test: /\.jsx?/,
-      loader: 'babel-loader'
-    }]
-  }
+    rules: [
+      {
+        test: /\.jsx?/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.module\.s(a|c)ss$/,
+        loader: [
+          isDevelopment ? 'style-loader' : MiniCssExtractPlugin.loader, {
+            loader: 'css-loader',
+            options: {
+              modules: true,
+              sourceMap: isDevelopment
+            }
+          }, {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: isDevelopment
+            }
+          }
+        ]
+      }, {
+        test: /\.s(a|c)ss$/,
+        exclude: /\.module.(s(a|c)ss)$/,
+        loader: [
+          isDevelopment ? 'style-loader' : MiniCssExtractPlugin.loader,
+          'css-loader', {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: isDevelopment
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: isDevelopment ? '[name].css' : '[name].[hash].css',
+      chunkFilename: isDevelopment ? '[id].css' : '[id].[hash].css'
+    })
+  ]
 }
