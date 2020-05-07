eae2b25bd3052d6dcecabaf0be9f31e46624f23e
Francisco Benedict <franciscobenedict@Franciscos-office-iMac-Conrad-Office.local>
2020-05-07 19:58:27 +0100

    added responsive CSS grid system for the landing page.

--- src/app/components/LandingPage.jsx
+++ src/app/components/LandingPage.jsx
@@ -44,57 +44,103 @@ export const LandingPage = ({authenticateUser, authenticated})=> {
   const [show, setShow] = useState(false);
   return (
     <div className="main fullscreen">
-      <h1>Landing Page</h1>
-      <p className="">Welcome to the Duchess App</p>
-      <div>
-        {(localStorage.getItem('authenticatedUser') !== "AUTHENTICATED") &&<Link to="/mainhome">Enter site</Link>}
-        {(localStorage.getItem('authenticatedUser') === "AUTHENTICATED") &&<Link to="/home">Enter site</Link>}
-      </div>
-      <div>
-        <Link to="/about">What The Duchess is about</Link>
-      </div>
-      <div>
-        <Link to="/dashboard">Dashboard</Link>
-      </div>
-      <img src={bird} alt="this is an image" />
+      <div className="grid_container_landing">
+        <div className="landing_grid_item1">
+          <div className="duchess_logo_container">
+            <img className="" src="" alt="The Duchess Logo" />
+          </div>
 
-      <div>
-        <div>1 ==> localStorage.getItem('authenticatedUser'): {localStorage.getItem('authenticatedUser')}</div>
-        <div>2 ==> authenticated: {authenticated}{!authenticated}</div>
+          {
+            (authenticated) &&
+            <div>
+              <div>Hello, "{localStorage.getItem('username')}"</div>
+              <div>You are free to browse The Duchess</div>
+              <div>
+                <div className="button_container">
+                  <Link className="btn btn-primary btn_anchor" to="/home">Browse</Link>
+                </div>
+              </div>
+            </div>
+          }
 
-        {(authenticated) && <div>You are currently logged in as "{localStorage.getItem('username')}"</div>}
-        { (!authenticated) &&
-          <div>
-            You are not logged in.
-            Please <a onClick={() => setShow(true)}>login</a>
+          { (!authenticated) &&
             <div>
               <Button variant="primary" onClick={() => setShow(true)}>
                 Login
               </Button>
             </div>
+          }
+        </div>
+        <div className="landing_grid_item2">2</div>
+        <div className="landing_grid_item3">3</div>
+        <div className="landing_grid_item4">4</div>
+        <div className="landing_grid_item5">5</div>
+        <div className="landing_grid_item6">
+          <div className="social_icons">
           </div>
-        }
-        <Modal
-          show={show}
-          onHide={() => setShow(false)}
-          dialogClassName="modal-90w"
-          aria-labelledby="example-custom-modal-styling-title"
-        >
-          <Modal.Header closeButton>
-            <Modal.Title id="example-custom-modal-styling-title">
-              Please login
-            </Modal.Title>
-          </Modal.Header>
-          <Modal.Body>
-            <form className="form" onSubmit={authenticateUser}>
-              <input className="form-control" type="text" placeholder="username" name="username" autoComplete="off" />
-              <input className="form-control mt-2" type="password" placeholder="password" defaultValue="" name="password" autoComplete="off" />
-              { authenticated === mutations.NOT_AUTHENTICATED ? <p className="error_text">Login incorrect</p> : null }
-              <button className="form-control mt-2 btn btn-primary" type="submit">Login</button>
-            </form>
-          </Modal.Body>
-        </Modal>
+        </div>
       </div>
+
+
+
+
+
+
+      {/*
+      //
+      // <h1>Landing Page</h1>
+      // <p className="">Welcome to the Duchess App</p>
+      // <div>
+      //   {(localStorage.getItem('authenticatedUser') !== "AUTHENTICATED") &&<Link to="/mainhome">Enter site</Link>}
+      //   {(localStorage.getItem('authenticatedUser') === "AUTHENTICATED") &&<Link to="/home">Enter site</Link>}
+      // </div>
+      // <div>
+      //   <Link to="/about">What The Duchess is about</Link>
+      // </div>
+      // <div>
+      //   <Link to="/dashboard">Dashboard</Link>
+      // </div>
+      // <img src={bird} alt="this is an image" />
+      //
+      // <div>
+      //   <div>1 ==> localStorage.getItem('authenticatedUser'): {localStorage.getItem('authenticatedUser')}</div>
+      //   <div>2 ==> authenticated: {authenticated}{!authenticated}</div>
+      //
+      //   {(authenticated) && <div>You are currently logged in as "{localStorage.getItem('username')}"</div>}
+      //   { (!authenticated) &&
+      //     <div>
+      //       You are not logged in.
+      //       Please <a onClick={() => setShow(true)}>login</a>
+      //       <div>
+      //         <Button variant="primary" onClick={() => setShow(true)}>
+      //           Login
+      //         </Button>
+      //       </div>
+      //     </div>
+      //   }
+    */}
+
+
+      <Modal
+        show={show}
+        onHide={() => setShow(false)}
+        dialogClassName="modal-90w"
+        aria-labelledby="example-custom-modal-styling-title"
+      >
+        <Modal.Header closeButton>
+          <Modal.Title id="example-custom-modal-styling-title">
+            Please login
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <form className="form" onSubmit={authenticateUser}>
+            <input className="form-control" type="text" placeholder="username" name="username" autoComplete="off" />
+            <input className="form-control mt-2" type="password" placeholder="password" defaultValue="" name="password" autoComplete="off" />
+            { authenticated === mutations.NOT_AUTHENTICATED ? <p className="error_text">Login incorrect</p> : null }
+            <button className="form-control mt-2 btn btn-primary" type="submit">Login</button>
+          </form>
+        </Modal.Body>
+      </Modal>
     </div>
   )
 };
