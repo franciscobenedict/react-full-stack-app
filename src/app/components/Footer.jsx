5a6b48ae1f49d96a95c15ee53498ca6cbac9ba18
Francisco Benedict <franciscobenedict@Franciscos-office-iMac-Conrad-Office.local>
2020-05-07 16:49:45 +0100

    styled footer links and footer layout in larger view and in small view; styled hover state and off_hover state of scroll-to-top button

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -12,26 +12,32 @@ const Footer = ()=> (
 
             {/* COPYRIGHT  */}
             <div className="col-12 col-md-12 col-lg-4">
-              <div className="copyright">Copyright &copy; 2020 | The Duchess App</div>
+              <div className="align_middle_container _100 _0 copyright_container">
+                <div className="align_content_middle">Copyright &copy; 2020 | The Duchess App</div>
+              </div>
             </div>
 
             {/* SOCIALS */}
             <div className="col-12 col-md-12 col-lg-4">
               <div className="social-icons">
-                <Link className="social-icon" to="/"><FontAwesomeIcon icon={['fab', 'youtube']} size="4x" /></Link>
-                <Link className="social-icon" to="/"><FontAwesomeIcon icon={['fab', 'facebook']} size="4x" /></Link>
-                <Link className="social-icon" to="/"><FontAwesomeIcon icon={['fab', 'instagram']} size="4x" /></Link>
-                <Link className="social-icon" to="/"><FontAwesomeIcon icon={['fab', 'twitter']} size="4x" /></Link>
+                <Link className="social-icon" to="/"><FontAwesomeIcon icon={['fab', 'youtube']} size="2x" /></Link>
+                <Link className="social-icon" to="/"><FontAwesomeIcon icon={['fab', 'facebook']} size="2x" /></Link>
+                <Link className="social-icon" to="/"><FontAwesomeIcon icon={['fab', 'instagram']} size="2x" /></Link>
+                <Link className="social-icon" to="/"><FontAwesomeIcon icon={['fab', 'twitter']} size="2x" /></Link>
               </div>
             </div>
 
             {/* FOOTER LINKS */}
             <div className="col-12 col-md-12 col-lg-4">
-              <div className="footer_links">
-                <ul>
-                  <li><Link to="/termsandconditions">Terms and Conditions</Link></li>
-                  <li><Link to="/privacypolicy">Privacy Policy</Link></li>
-                </ul>
+              <div className="align_middle_container _100 _0 footer_links_container">
+                <div className="align_content_middle">
+                  <div className="footer_links">
+                    <ul>
+                      <li><Link to="/termsandconditions">Terms and Conditions</Link></li>
+                      <li><Link to="/privacypolicy">Privacy Policy</Link></li>
+                    </ul>
+                  </div>
+                </div>
               </div>
             </div>
 
